39d5ede835b104381fea704a9e1045d39be02fa5
John Gile <jgile@cliquestudios.com>
2019-01-03 09:50:58 -0600

    ignore

--- packages/stub-vendor/stub-package/src/StubPackageServiceProvider.php
+++ packages/stub-vendor/stub-package/src/StubPackageServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace StubVendor\StubPackage;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class StubPackageServiceProvider extends ServiceProvider
@@ -11,19 +12,91 @@ class StubPackageServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/stub-package.php' => config_path('stub-package.php'),
-            ], 'config');
+        /*
+        |--------------------------------------------------------------------------
+        | Config
+        |--------------------------------------------------------------------------
+        */
+        $this->publishes([
+            __DIR__ . '/../config/stub-package.php' => config_path('stub-package.php'),
+        ], 'config');
 
-            /*
-            $this->loadViewsFrom(__DIR__.'/../resources/views', 'stub-package');
 
-            $this->publishes([
-                __DIR__.'/../resources/views' => base_path('resources/views/vendor/stub-package'),
-            ], 'views');
-            */
-        }
+        /*
+        |--------------------------------------------------------------------------
+        | Directives
+        |--------------------------------------------------------------------------
+        */
+//        Blade::directive('datetime', function ($expression) {
+        /*            return "<?php echo ($expression)->format('m/d/Y H:i'); ?>";*/
+//        });
+//
+//        Blade::if('env', function ($environment) {
+//            return app()->environment($environment);
+//        });
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Migrations
+        |--------------------------------------------------------------------------
+        */
+//        $this->loadMigrationsFrom(__DIR__ . '/path/to/migrations');
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Translations
+        |--------------------------------------------------------------------------
+        */
+
+//        $this->loadTranslationsFrom(__DIR__ . '/path/to/translations', 'stub-package');
+//        $this->publishes([
+//            __DIR__ . '/path/to/translations' => resource_path('lang/vendor/stub-package'),
+//        ]);
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Routes
+        |--------------------------------------------------------------------------
+        */
+//        $this->loadRoutesFrom(__DIR__ . '/routes.php');
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Views
+        |--------------------------------------------------------------------------
+        */
+//        $this->loadViewsFrom(__DIR__ . '/path/to/views', 'stub-package');
+//        $this->publishes([
+//            __DIR__ . '/path/to/views' => resource_path('views/vendor/stub-package'),
+//        ]);
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Commands
+        |--------------------------------------------------------------------------
+        */
+//        if ($this->app->runningInConsole()) {
+//            $this->commands([
+//                FooCommand::class,
+//                BarCommand::class,
+//            ]);
+//        }
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Public Assets
+        |--------------------------------------------------------------------------
+        | php artisan vendor:publish --tag=public --force
+        */
+//        $this->publishes([
+//            __DIR__.'/path/to/assets' => public_path('vendor/stub-package'),
+//        ], 'public');
     }
 
     /**
@@ -31,6 +104,6 @@ class StubPackageServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'stub-package');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'stub-package');
     }
 }
